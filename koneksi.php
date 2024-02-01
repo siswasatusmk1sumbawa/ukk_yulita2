fc06afb5ba76283e4f25ffcba704a4e162dabbc2
siswasatu <157083585+siswasatusmk1sumbawa@users.noreply.github.com>
2024-02-01 08:18:17 +0800

    catatan register
    koneksi.php
    proses_register.php
    register.php

---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php
+$servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "ukk_yulita";
+
+  // Create connection
+  $conn = mysqli_connect($servername, $username, $password, $dbname);
+?>
